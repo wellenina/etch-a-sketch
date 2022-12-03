c4968331a4422fe77c4f9fc2e1fc2f36d6597c1c
wellenina <giulia.fanzaghi@gmail.com>
2022-12-03 18:01:54 +0100

    Fix color picker for background and hovering effect on cells

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const DEFAULT_BACKGROUND_COLOR = '#ffffff';
 const DEFAULT_MODE = 'color'; // color, rainbow, grayscale, eraser
 
 let gridSize = DEFAULT_GRID_SIZE;
-let selectedColor = DEFAULT_BRUSH_COLOR;
-let bgColor = DEFAULT_BACKGROUND_COLOR;
 let currentMode = DEFAULT_MODE;
 
 const grid = document.getElementById('grid');
@@ -16,104 +14,109 @@ window.addEventListener('mouseup', () => {isMousedown = false;});
 // color pickers
 const brushColorPicker = document.getElementById('brush-color');
 const bgColorPicker = document.getElementById('bg-color');
-brushColorPicker.value = selectedColor;
-bgColorPicker.value = bgColor;
+brushColorPicker.value = DEFAULT_BRUSH_COLOR;
+bgColorPicker.value = DEFAULT_BACKGROUND_COLOR;
 brushColorPicker.addEventListener('change', selectBrushColor);
 bgColorPicker.addEventListener('change', selectBgColor);
 
+// mode buttons
 const modeBtn = {
-  color: document.getElementById('color'),
-  rainbow: document.getElementById('rainbow'),
-  grayscale: document.getElementById('grayscale'),
-  eraser: document.getElementById('eraser')
+    color: document.getElementById('color'),
+    rainbow: document.getElementById('rainbow'),
+    grayscale: document.getElementById('grayscale'),
+    eraser: document.getElementById('eraser')
 }
-
 for (const key in modeBtn) {
-  modeBtn[key].addEventListener('click', changeMode);
+    modeBtn[key].addEventListener('click', changeMode);
 }
 
+const colorManager = {
+    rainbowColors: ['#F1CEF4', '#E4CFFF', '#C1CCFF', '#B9E3FF', '#A6FFD3', '#BCFFA4', '#F8FF97', '#FFE5A8', '#E8CACA'],
+    grayscaleColors: ['#e6e6e6', '#bfbfbf', '#999999', '#737373', '#4d4d4d', '#262626', '#000000', '#262626', '#4d4d4d', '#737373', '#999999', '#bfbfbf'],
+    counter: 0,
+  
+    color: DEFAULT_BRUSH_COLOR,
+    rainbow: '#F1CEF4',
+    grayscale: '#e6e6e6',
+    eraser: DEFAULT_BACKGROUND_COLOR,
+
+    updateColor() {
+        const colors = this[`${currentMode}Colors`];
+        const nextColor = colors[this.counter++ % colors.length];
+        setHoverCellColor(nextColor);
+        this[currentMode] = nextColor;
+        },
+
+    resetColors() {
+        this.counter = 0;
+        this.rainbow = '#F1CEF4';
+        this.grayscale = '#e6e6e6';
+    }
+}
+
+  
 const clearBtn = document.getElementById('clear-btn');
 clearBtn.addEventListener('click', clear);
 
 //// slider - grid size --> not working yet
 
-const rainbowColors = ['#F1CEF4', '#E4CFFF', '#C1CCFF', '#B9E3FF', '#A6FFD3', '#BCFFA4', '#F8FF97', '#FFE5A8', '#E8CACA'];
-const grayColors = ['#e6e6e6', '#bfbfbf', '#999999', '#737373', '#4d4d4d', '#262626', '#000000', '#262626', '#4d4d4d', '#737373', '#999999', '#bfbfbf'];
-let colorCounter = 0;
 
-/////// AGGIUSTARE QUESTA PARTE ///////////////
 function changeCellColor(event) {
+    if (event.type === 'mouseenter' && !isMousedown) { return; };
     const cell = event.target;
-
-    if (!isMousedown) { // hover effect
-        const cellColor = cell.style.backgroundColor;
-        cell.addEventListener('mouseout', () => {
-            cell.style.backgroundColor = cellColor;
-        }, { once: true });
-    }
-
-    cell.style.backgroundColor = getColor(currentMode);
+    currentMode === 'eraser' ? cell.className = 'empty-cell' : cell.removeAttribute('class', 'empty-cell');
+    cell.style.backgroundColor = colorManager[currentMode];
+    if (currentMode === 'rainbow' || currentMode === 'grayscale') { colorManager.updateColor(); }
 }
 
 
-function getColor(mode) {
-
-  if (mode === 'color') {
-    return selectedColor;
-  }
-
-  if (mode === 'eraser') {
-    return bgColor;
-  }
-
-  if (mode === 'rainbow') {
-    return rainbowColors[colorCounter++ % rainbowColors.length];
-  }
-
-  if (mode === 'grayscale') {
-    return grayColors[colorCounter++ % grayColors.length];
-  }
+function setHoverCellColor(newColor) {
+    const r = document.querySelector(':root');
+    r.style.setProperty('--hover-cell-color', newColor);
 }
 
-/////// FINE PARTE DA AGGIUSTARE ///////////////
 
 function createGrid() {
-  for (let i = 0; i < (gridSize * gridSize); i++) {
-    const cell = document.createElement('div');
-    cell.addEventListener('mouseenter', changeCellColor);
-    cell.style.backgroundColor = bgColor;
-    cell.className = 'bg-cell';
-    grid.appendChild(cell);
-  };
-  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-  grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const cell = document.createElement('div');
+        ['mouseenter', 'mousedown'].forEach(event => cell.addEventListener(event, changeCellColor));
+        cell.style.backgroundColor = DEFAULT_BACKGROUND_COLOR;
+        cell.className = 'empty-cell';
+        grid.appendChild(cell);
+    };
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 }
 
 
 function selectBrushColor(event) {
-    selectedColor = event.target.value;
-  }
-  
-  function selectBgColor(event) {
+    const selectedColor = event.target.value;
+    colorManager.color = selectedColor;
+    if (currentMode === 'color') { setHoverCellColor(selectedColor); };
+}
+
+function selectBgColor(event) {
     newBgColor = event.target.value;
-    bgColor =  newBgColor; // cambia variabile globale per le prossime volte
-    document.querySelectorAll('.bg-cell').forEach(cell => cell.style.backgroundColor = newBgColor);
-  }
+    colorManager.eraser =  newBgColor;
+    if (currentMode === 'eraser') { setHoverCellColor(newBgColor); };
+    document.querySelectorAll('.empty-cell').forEach(cell => cell.style.backgroundColor = newBgColor);
+}
 
 
 function changeMode(event) {
-  let newMode = event.target.id;
+    let newMode = event.target.id;
+    if (currentMode === newMode) { return; };
 
-  if (currentMode === newMode) { return; };
-
-  modeBtn[currentMode].classList.toggle('active');
-  modeBtn[newMode].classList.toggle('active');
-  currentMode = newMode;
+    modeBtn[currentMode].classList.toggle('active');
+    modeBtn[newMode].classList.toggle('active');
+    currentMode = newMode;
+    colorManager.resetColors();
+    setHoverCellColor(colorManager[currentMode]);
 }
 
 
 function clear() {
-  Array.from(grid.children).forEach(cell => cell.style.backgroundColor = bgColor);
+    Array.from(grid.children).forEach(cell => {cell.style.backgroundColor = colorManager.eraser; cell.className = 'empty-cell'} );
 }
 
 
